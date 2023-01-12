a3d220616036cae3ca94c752f0c0f41881180597
Zhuangsccc <469856433@qq.com>
2023-01-12 10:18:28 +0800

    fear:完成学生基本信息分页功能

--- router_handler/vue3_user.js
+++ router_handler/vue3_user.js
@@ -58,13 +58,25 @@ exports.getRoutes= (req,res)=>{
     })
 }
 exports.getStuInfo=(req,res)=>{
-    const sqlStr = "SELECT * FROM `stu_user` WHERE 1 order by major,class asc"
-    db.query(sqlStr,(err,result)=>{
+    let {pageIndex,pageSize} = req.query
+    pageIndex = parseInt(pageIndex)
+    pageSize = parseInt(pageSize)
+    let total = 0
+    const getTotal = "select count(id) as tt from examinations"
+    const sqlStr = "SELECT * FROM `stu_user` WHERE 1 order by major,class asc limit ?,?"
+    db.query(getTotal,(err,newTotal)=>{
+        if(err) res.cc(err)
+        total = newTotal[0].tt
+    })
+    db.query(sqlStr,[pageIndex,pageSize],(err,result)=>{
         if(err) res.cc(err)
         res.send({
             code:200,
             msg:"查询成功",
-            data:result
+            data:{
+                tableData:result,
+                total,
+            }
         })
     })
 }
