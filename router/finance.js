de1a370664e64eb4f65ef88f1123fd8bae3487f7
Zhuangsccc <469856433@qq.com>
2023-01-13 11:53:20 +0800

    fear:新增财务

--- router/finance.js
+++ router/finance.js
@@ -2,4 +2,6 @@ const express = require("express")
 const router = express.Router()
 const finance_handler =require("../router_handler/finance.js")
 router.get("/getList",finance_handler.getList)
+router.get("/getApprovalList",finance_handler.getApprovalList)
+router.post("/addFinance",finance_handler.addFinance)
 module.exports = router
--- router_handler/finance.js
+++ router_handler/finance.js
@@ -22,3 +22,38 @@ exports.getList=(req,res)=>{
         })
     })
 }
+exports.getApprovalList=(req,res)=>{
+    let {pageIndex,pageSize} = req.query
+    pageIndex = parseInt(pageIndex)
+    pageSize = parseInt(pageSize)
+    let total = 0
+    const getTotal = "select count(id) as tt from finance where state=0"
+    db.query(getTotal,(err,newTotal)=>{
+        if(err) res.cc(err)
+        total = newTotal[0].tt
+    })
+    const sqlStr = "SELECT * FROM `finance` WHERE state=0 order by time desc  limit ?,?"
+    db.query(sqlStr,[pageIndex,pageSize],(err,result)=>{
+        if(err) res.cc(err)
+        res.send({
+            code:200,
+            msg:"查询成功",
+            data:{
+                tableData:result,
+                total,
+            }
+        })
+    })
+}
+exports.addFinance=(req,res)=>{
+    const {price,time,title,submitter,type,state,approver} = req.body
+    const sqlStr = "INSERT INTO `finance`(`price`, `time`, `title`, `submitter`, `type`, `state`, `approver`) VALUES (?,?,?,?,?,?,?)"
+    db.query(sqlStr,[price,time,title,submitter,type,state,approver],(err,result)=>{
+        if (err) return res.cc(err)
+        if (result.affectedRows === 1) {
+            res.cc("新增成功", 200)
+        } else {
+            res.cc("新增失败")
+        };
+    })
+}
